00605fdbaf0addb7760719ce7b85c59358bd0a00
Shrutisingh9 <tanukum.ss784@gmail.com>
2026-01-13 13:39:53 +0530

    basic practice question

---
+++ level_o/basic question.py
@@ -0,0 +1,85 @@
+print("Hello Python")
+
+# Q2 print your name, college, and course on different lines.
+
+# name
+print("Shruti Singh")
+# college
+print("GITM")
+# course
+print("B.Tech CSE")
+
+# variables
+# example of variable assignment
+a = 10
+name="shruti"
+age="21"
+marks=89.5
+print(a)
+print(name)
+print(age)
+print(marks) 
+
+# Q3 create variables for name, age, city and print them
+my_name = "Shruti Singh"
+my_age = 21
+my_city = "Delhi" 
+print(my_name)
+print(my_age)
+print(my_city)
+
+# Q4 Identify datatypes of: 25, 3.14, "india", False
+print(type(25))       # int
+print(type(3.14))     # float
+print(type("india"))  # str
+print(type(False))    # bool    
+
+# Q5 create one variable ofeach datatypes
+var_int = 100               # int
+var_float = 9.81            # float
+var_str = "Hello, World!"   # str
+var_bool = True             # bool   
+print(var_int)   
+print(type(var_int))
+print(var_float)   
+print(type(var_float))
+print(var_str)   
+print(type(var_str))
+print(var_bool)   
+print(type(var_bool))   
+
+# Q6 take username and print a welcome message
+username = input("Enter your name: ")
+print("Welcome, " + username + "!")
+
+# Q7 take two numbers from user and print them
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+print("First number:", num1)
+print("Second number:", num2)
+
+# Q8 write a program to add two numbers.
+num1=input("Enter first number:")
+num2=input("Enter second number:")
+sum=float(num1)+float(num2)
+print("the sum of two numbers is:",sum)
+
+# Q9 find reminder when 15 is divided by 4.
+reminder = 15%4
+print("The reminder when 15 is divided by 4 is:", reminder)
+
+# Q10 multiply two user input number.
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+product = float(num1) * float(num2)
+print("The product of two numbers is:", product)
+
+# Q11 convert float to int.
+float_num = 5.7
+int_num = int(float_num)
+print("The integer value of", float_num, "is", int_num)
+
+# Q12 convert integer to string.
+int_value = 10
+str_value = str(int_value)
+print("The string value of", int_value, "is", str_value)  
