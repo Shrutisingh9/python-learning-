f7caee4c0d88224d959c856785dff0a930bf22c3
Shrutisingh9 <tanukum.ss784@gmail.com>
2026-01-15 21:40:42 +0530

    tuple

---
+++ level_1/3. data structure/2. tuple.py
@@ -0,0 +1,21 @@
+# tuple
+days = ("Mon", "Tue", "Wed")
+
+# Accessing Elements
+print(days[0])
+
+# Tuple Packing & Unpacking
+a, b, c = days
+print(a, b, c)
+
+# Practice Questions
+# Create tuple of months
+months = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+print(months)
+
+# Access last element
+print(months[-1])
+
+# Convert tuple to list
+months_list = list(months)
+print(months_list)
