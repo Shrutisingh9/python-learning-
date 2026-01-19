528c3a9fb4ab27a0d9c2fcef13e086bd14e08c66
Shrutisingh9 <tanukum.ss784@gmail.com>
2026-01-19 12:15:17 +0530

    modules

--- level_2/2. modules.py
+++ level_2/2. modules.py
@@ -46,3 +46,63 @@ print(now.time())
 import sys
 print(sys.version)
 print(sys.path)
+
+# practicalexamples
+# Example 1: Random Password Generator
+import random
+password = random.randint(1000, 9999)
+print(password)
+
+# Example 2: Date Difference
+from datetime import date
+d1 = date(2024, 1, 1)
+d2 = date.today()
+print(d2 - d1)
+
+# PRACTICE QUESTIONS
+# Import math module and find square root of 64.
+import math
+print(math.sqrt(64))
+
+# Use random module to generate a number between 1 and 50.
+import random
+print(random.randint(1, 50))
+
+# Create your own module with functions:
+# add
+def add(a, b):
+    return a+b
+
+# multiply
+def mul(a, b):
+    return a*b
+
+# Import only one function from math module.
+from math import sqrt
+print(sqrt(81))
+
+# Create a module to check:
+
+# prime number
+def prime(n):
+    if n <=1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n %i ==0:
+            return False
+    return True
+
+n = int(input("enter a number: "))
+if prime(n):
+    print(n, "is prime number")
+else:
+    print(n, "is not prime number")
+
+# palindrome number
+def palindrome(n):
+    return str(n) == str(n)[::-1]
+n = int(input("enter a number: "))
+if palindrome(n):
+    print(n, "is palindrome")
+else:
+    print(n, "is not palindrome")
---
+++ level_2/user defined modules/main.py
@@ -0,0 +1,6 @@
+import calculator
+
+print(calculator.add(10, 5))
+print(calculator.sub(10, 5))
+print(calculator.mul(10, 5))
+print(calculator.div(10, 5))
